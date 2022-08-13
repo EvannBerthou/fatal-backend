d3fbf8e0403a4666bab2ceabf7ad99b6e008c118
Evann Berthou <evann.berthou@gmail.com>
2022-08-13 12:28:14 +0200

    Ajout de la lib Devise pour gérer l'auth
    Les routes à utiliser pour tester la lib:
    - http://localhost:3000/users/sign_up
    - http://localhost:3000/users/sign_in
    - http://localhost:3000/logged_in => Boolean pour voir si on est connecté ou non
    
    Ces pages sont là pour les tests et devront être remplacées par le front Angular

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,8 @@ class UserController < ApplicationController
     def home
         render json: User.all
     end
+
+    def logged_in?
+        render json: user_signed_in?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/users', to: 'user#home'
+  get '/logged_in', to: 'user#logged_in?'
 end
