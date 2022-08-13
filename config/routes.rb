c5de9080a1caf07b06ad0b30c0dbdfbd6107a9a0
Evann Berthou <evann.berthou@gmail.com>
2022-08-13 13:20:19 +0200

    Ajout des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+default_actions = [:create, :show, :update, :destroy]
 Rails.application.routes.draw do
   devise_for :users
   get '/users', to: 'user#home'
   get '/logged_in', to: 'user#logged_in?'
+
+  resource :qcm, only: default_actions
+  resource :question, only: default_actions
+  resource :reponse, only: default_actions
+  resource :etudiant, only: default_actions
+  resource :admin
 end
