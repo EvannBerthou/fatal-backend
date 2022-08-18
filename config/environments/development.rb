066d5c7df087694f7c1f15c1f0245363489b161b
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-08-18 10:02:38 +0200

    Ajout de l'hôte distant aux hôtes autorisés

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Authorize connexions from the right url
+  config.hosts = "back.fatal.krapo.pro"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
