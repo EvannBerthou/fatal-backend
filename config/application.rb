9db24a611bbd2008a2ee78808f09b120fa31d5f2
Evann Berthou <evann.berthou@gmail.com>
2022-09-03 17:39:35 +0200

    Config pour les sessions
    J'ai rien capté mais fallait faire ça pour pouvoir faire les requêtes depuis le back

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module FatalBack
     config.api_only = true
     config.debug_exception_response_format = :api
 
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
