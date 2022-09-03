b67bcf32991de9f569861852a170964686b7168b
Evann Berthou <evann.berthou@gmail.com>
2022-09-03 15:57:45 +0200

    Passage en mode API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  wrap_parameters format: []
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,8 +10,8 @@ module FatalBack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    #config.api_only = true
-    #config.debug_exception_response_format = :api
+    config.api_only = true
+    config.debug_exception_response_format = :api
 
     # Configuration for the application, engines, and railties goes here.
     #
