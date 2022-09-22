1aa411b2de5997981d23b5001043432d4c0a824d
Krapoviche <63341122+Krapoviche@users.noreply.github.com>
2022-09-22 22:15:05 +0200

    Ajout de http://front.fatal.krapo.pro aux CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,4 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:4200'
     resource '*', headers: :any, methods: :any, credentials: true, expose: ['Authorization']
   end
+  allow do
+    origins 'http://front.fatal.krapo.pro'
+    resource '*', headers: :any, methods: :any, credentials: true, expose: ['Authorization']
+  end
 end
