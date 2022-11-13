1a850f7d9064ddc51d8f6e9975026e129dff76a0
Krapo <telliemilien@gmail.com>
2022-11-13 20:25:29 +0100

    Revert "(#26) Simplification de l'utilisation des envvars"
    This reverts commit 4f80ae5ec1daa61cd827a30eeb5a701227f43d31.
    
    Removing errors caused by ruby environment vars

--- lib/bash_module.rb
+++ lib/bash_module.rb
@@ -1,18 +1,17 @@
 module BashModule
     private
-    save_folder = Rails.configuration.amc[:save_folder]
 
     def AMC_create_dir(qcm)
-        `mkdir #{save_folder}/#{qcm.id} #{save_folder}/#{qcm.id}/data`
+        `mkdir #{Rails.configuration.amc[:save_folder]}/#{qcm.id} #{Rails.configuration.amc[:save_folder]}/#{qcm.id}/data`
     end
 
     def AMC_write_qcm_file(qcm)
       `echo "#{qcm.toTxt}" \
-      > #{save_folder}/#{qcm.id}/#{qcm.id}.txt`
+      > #{Rails.configuration.amc[:save_folder]}/#{qcm.id}/#{qcm.id}.txt`
     end
 
     def AMC_generation(qcm)
-        `cd #{save_folder}/#{qcm.id}/ && auto-multiple-choice prepare --mode s \
+        `cd #{Rails.configuration.amc[:save_folder]}/#{qcm.id}/ && auto-multiple-choice prepare --mode s \
         --prefix . \
         #{qcm.id}.txt \
         --out-sujet DOC-sujet.pdf \
@@ -23,7 +22,7 @@ module BashModule
     end
 
     def AMC_move_pdf_to_downloadable_folder(qcm)
-        `mv #{save_folder}/#{qcm.id}/DOC-sujet.pdf \
-        #{save_folder}/#{qcm.id}.pdf`
+        `mv #{Rails.configuration.amc[:save_folder]}/#{qcm.id}/DOC-sujet.pdf \
+        #{Rails.configuration.amc[:pdf_folder]}/#{qcm.id}.pdf`
     end
 end
